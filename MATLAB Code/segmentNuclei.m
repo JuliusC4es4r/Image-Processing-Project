1519498997eb497ebffc24e686bea5a0ef5c63ee
JuliusC4es4r <47398230+JuliusC4es4r@users.noreply.github.com>
2023-03-19 22:05:57 -0700

    WIP segment nuclei. Untested yet

--- MATLAB Code/segmentNuclei.m
+++ MATLAB Code/segmentNuclei.m
@@ -29,3 +29,57 @@ title('Original');
 subplot(1,2,2);
 imshow(filteredImage, []);
 title('Filtered');
+
+% function N = nucleus_segmentation(EDF_image, m, s, t1, t2, d)
+%     % Initialization
+%     N = zeros(size(EDF_image));
+%     I = wiener2(EDF_image);
+% 
+%     % Iterative detection and segmentation
+%     for t = t1:10:t2
+%         B = I <= t;
+%         CC = bwconncomp(B);
+%         stats = regionprops(CC, 'Area', 'Solidity', 'Image');
+% 
+%         for i = 1:length(stats)
+%             r = stats(i);
+%             r_size = r.Area;
+%             r_solidity = r.Solidity;
+%             r_mask = zeros(size(B));
+%             r_mask(CC.PixelIdxList{i}) = 1;
+% 
+%             if r_size < m || r_solidity < s
+%                 % Remove r
+%                 B(CC.PixelIdxList{i}) = 0;
+%             else
+%                 overlap_mask = r_mask & N;
+%                 if sum(overlap_mask(:)) == 0
+%                     N = N | r_mask;
+%                 else
+%                     overlap_stats = regionprops(overlap_mask, 'Solidity');
+%                     max_overlap_solidity = max([overlap_stats.Solidity]);
+% 
+%                     if r_solidity >= max_overlap_solidity
+%                         N = N | r_mask;
+%                     end
+%                 end
+%             end
+%         end
+%     end
+% 
+%     % Post-processing
+%     N = imdilate(N, strel('disk', 1));
+%     CC = bwconncomp(N);
+%     stats = regionprops(CC, EDF_image, 'PixelValues', 'Perimeter', 'PixelList');
+% 
+%     for i = 1:length(stats)
+%         r = stats(i);
+%         r_intensity = mean(r.PixelValues);
+%         outer_boundary = bwperim(r.Perimeter);
+%         outer_boundary_intensity = mean(EDF_image(sub2ind(size(EDF_image), outer_boundary(:, 2), outer_boundary(:, 1))));
+% 
+%         if r_intensity - outer_boundary_intensity < d
+%             N(CC.PixelIdxList{i}) = 0;
+%         end
+%     end
+% end
