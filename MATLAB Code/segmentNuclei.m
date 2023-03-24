35e3046a552cee3d3c4b9f086d38a1f1a524bd06
JuliusC4es4r <47398230+JuliusC4es4r@users.noreply.github.com>
2023-03-23 22:41:50 -0700

    Completed segment nuclei function
    Need to add notes

--- MATLAB Code/segmentNuclei.m
+++ MATLAB Code/segmentNuclei.m
@@ -1,4 +1,4 @@
-function  N = segmentNuclei(imgIn, minNucleus, minSolidity, t1, t2, minArea, d)
+function N = segmentNuclei(EDF_image, m, s, t1, t2, d)
 % imgIn = 
 % minNucleus = 
 % minSolidity = 
@@ -12,14 +12,100 @@ function  N = segmentNuclei(imgIn, minNucleus, minSolidity, t1, t2, minArea, d)
 % paper "A framework for nucleus and overlapping cytoplasm..."
 % Miguel Manguerra and Kimiya Mousavi
 
-% Reading image
-imageSample = imread(imgIn);
+    % Initialization
+    imageSample = imread(EDF_image);
 
-% Converting image to grayscale
-imageSample = im2gray(imageSample);
+    % Converting image to grayscale
+    imageSample = im2gray(imageSample);
 
-% weiner2 implements 2D adaptive noise removal filtering
-filteredImage = wiener2(imageSample, [3,3]);
+    imageSample = double(imageSample);
+
+    N = zeros(size(imageSample));
+    I = wiener2(imageSample);
+
+    % Iterative detection and segmentation
+    for t = t1:10:t2
+        B = I <= t;
+        CC = bwconncomp(B);
+        stats = regionprops(CC, 'Area', 'Solidity', 'Image');
+        
+        for i = 1:CC.NumObjects
+            r = stats(i);
+            r_size = r.Area;
+            r_solidity = r.Solidity;
+            r_mask = zeros(size(B));
+            r_mask(CC.PixelIdxList{i}) = 1;
+            overlap_mask = r_mask & N;
+            if r_size < m || r_solidity < s
+                % Remove r
+                B(CC.PixelIdxList{i}) = 0;
+            elseif (overlap_mask == 0)
+                
+                N = N | r_mask;
+            else
+                overlap_stats = regionprops(overlap_mask, 'Solidity');
+                max_overlap_solidity = max([overlap_stats.Solidity]);
+
+                if r_solidity >= max_overlap_solidity
+                    N = N | r_mask;
+                else
+                    B(CC.PixelIdxList{i}) = 0;
+                end
+            end
+        end
+    end
+
+    % Post-processing
+   
+    CC = bwconncomp(N);
+    stats = regionprops(CC, imageSample, 'PixelValues', 'Perimeter', 'PixelList');
+
+
+    for i = 1:CC.NumObjects
+        
+        r = stats(i);
+        r_mask = zeros(size(N));
+        r_mask(CC.PixelIdxList{i}) = 1;
+        r_mask = imdilate(r_mask, strel('disk', 1));
+
+        r_intensity = mean(r.PixelValues);
+        outer_boundary = bwperim(r_mask);
+%         outer_boundary_intensity = r.PixelList & outer_boundary;
+
+%         outer_boundary = bwperim(r.Perimeter);
+       x_values = zeros(1,1);
+       y_values = zeros(1,1);
+        
+        for k = 1:size(outer_boundary)
+            for j = 1:size(outer_boundary)
+                if (outer_boundary(k,j) == 1)
+                    if((x_values(1) == 0) && (y_values(1) == 0))
+                        x_values(1) = k;
+                        y_values(1) = j;
+                    else
+                        temp_x = x_values;
+                        temp_y = y_values;
+                        x_values = [temp_x, k];
+                        y_values = [temp_y, j];
+                    end
+                end
+            end
+        end
+
+        outer_boundary_sum = 0.0;
+        for k = 1:length(x_values)
+            outer_boundary_sum = outer_boundary_sum + imageSample(x_values(k), y_values(k));
+        end
+
+        outer_boundary_intensity = outer_boundary_sum/ length(x_values);
+% 
+%         if r_intensity - outer_boundary_intensity < d
+%             N(CC.PixelIdxList{i}) = 0;
+%         end
+        if (abs(r_intensity - outer_boundary_intensity)) < d
+            N(CC.PixelIdxList{i}) = 0;
+        end
+    end
 
 % Display the original image, noisy image, and filtered image
 figure;
@@ -27,59 +113,6 @@ subplot(1,2,1);
 imshow(imageSample, []);
 title('Original');
 subplot(1,2,2);
-imshow(filteredImage, []);
-title('Filtered');
-
-% function N = nucleus_segmentation(EDF_image, m, s, t1, t2, d)
-%     % Initialization
-%     N = zeros(size(EDF_image));
-%     I = wiener2(EDF_image);
-% 
-%     % Iterative detection and segmentation
-%     for t = t1:10:t2
-%         B = I <= t;
-%         CC = bwconncomp(B);
-%         stats = regionprops(CC, 'Area', 'Solidity', 'Image');
-% 
-%         for i = 1:length(stats)
-%             r = stats(i);
-%             r_size = r.Area;
-%             r_solidity = r.Solidity;
-%             r_mask = zeros(size(B));
-%             r_mask(CC.PixelIdxList{i}) = 1;
-% 
-%             if r_size < m || r_solidity < s
-%                 % Remove r
-%                 B(CC.PixelIdxList{i}) = 0;
-%             else
-%                 overlap_mask = r_mask & N;
-%                 if sum(overlap_mask(:)) == 0
-%                     N = N | r_mask;
-%                 else
-%                     overlap_stats = regionprops(overlap_mask, 'Solidity');
-%                     max_overlap_solidity = max([overlap_stats.Solidity]);
-% 
-%                     if r_solidity >= max_overlap_solidity
-%                         N = N | r_mask;
-%                     end
-%                 end
-%             end
-%         end
-%     end
-% 
-%     % Post-processing
-%     N = imdilate(N, strel('disk', 1));
-%     CC = bwconncomp(N);
-%     stats = regionprops(CC, EDF_image, 'PixelValues', 'Perimeter', 'PixelList');
-% 
-%     for i = 1:length(stats)
-%         r = stats(i);
-%         r_intensity = mean(r.PixelValues);
-%         outer_boundary = bwperim(r.Perimeter);
-%         outer_boundary_intensity = mean(EDF_image(sub2ind(size(EDF_image), outer_boundary(:, 2), outer_boundary(:, 1))));
-% 
-%         if r_intensity - outer_boundary_intensity < d
-%             N(CC.PixelIdxList{i}) = 0;
-%         end
-%     end
-% end
+imshow(N, []);
+title('Output Binary Mask N');
+end
