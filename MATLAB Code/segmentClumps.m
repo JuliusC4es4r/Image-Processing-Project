90236785bbaa7248300549072adcca9793681695
JuliusC4es4r <47398230+JuliusC4es4r@users.noreply.github.com>
2023-03-30 12:18:53 -0700

    added median shit

--- MATLAB Code/segmentClumps.m
+++ MATLAB Code/segmentClumps.m
@@ -9,6 +9,18 @@ function segmentedClumps = segmentClumps(EDF_image, nucleusMask, q, q_prime)
     imageSample = im2gray(imageSample);
 
     imageSample = double(imageSample);
+    binarizedImage = zeros(length(imageSample));
+    binarizedImage = double(binarizedImage);
+
+    imageMedian = median(imageSample(:));
+
+    for i = 1:length(imageSample)
+        for j = 1:length(imageSample)
+            if(imageSample(i,j) < imageMedian)
+                binarizedImage(i,j) = 1;
+            end
+        end
+    end
     GMM = fitgmdist(imageSample, 5);
    
 % Display the original image and output binary mask
