17c11ae85dc77f184989004e5d3d127c771adeb1
JuliusC4es4r <47398230+JuliusC4es4r@users.noreply.github.com>
2023-04-02 03:00:15 -0700

    Kinda working cytoplasm
    added ground truths for comparison. created nuclei mask comparison function for comparing GT to output nuclei

--- MATLAB Code/README.txt
+++ MATLAB Code/README.txt
@@ -1,4 +1,6 @@
- segmentNuclei("EDF000.png", 50, 0.9, 10, 180, 1.9) 
-almost perfect
-segmentClumps('EDF000.png', nucleusMask, 1000,218) 
-pretty good results
+run these to setup workspace:
+
+alpha = 2;
+W = 200;
+segmentedClumps = segmentClumps('EDF000.png', nucleusMask, 1000);
+nucleusMask = segmentNuclei("EDF000.png", 50, 0.95, 10, 210, 2.5);
--- MATLAB Code/segmentNuclei.m
+++ MATLAB Code/segmentNuclei.m
@@ -11,21 +11,29 @@ function N = segmentNuclei(EDF_image, m, s, t1, t2, d)
 % paper "A framework for nucleus and overlapping cytoplasm..."
 % Miguel Manguerra and Kimiya Mousavi
 
-    % Initialization
+    % Initialization of image
     imageSample = imread(EDF_image);
 
     % Converting image to grayscale
     imageSample = im2gray(imageSample);
 
+    % converting images to doubles for calculations
     imageSample = double(imageSample);
-
+    
+    % initializing nuclei mask
     N = zeros(size(imageSample));
+
+    % filtering with a 2d adaptive noise filter
     I = wiener2(imageSample);
 
     % Iterative detection and segmentation
     for t = t1:10:t2
         B = I <= t;
+
+        % separating all the B&W regions of the image
         CC = bwconncomp(B);
+
+        % finding the stats of those regions
         stats = regionprops(CC, 'Area', 'Solidity', 'Image');
         
         for i = 1:CC.NumObjects
@@ -35,16 +43,19 @@ function N = segmentNuclei(EDF_image, m, s, t1, t2, d)
             r_mask = zeros(size(B));
             r_mask(CC.PixelIdxList{i}) = 1;
             overlap_mask = r_mask & N;
+            % remove region if region is smaller than minimum area m or
+            % solidity is less than s
             if r_size < m || r_solidity < s
-                % Remove r
                 B(CC.PixelIdxList{i}) = 0;
             elseif (overlap_mask == 0)
-                
+                % adding mask back to nuclei mask
                 N = N | r_mask;
             else
+                % finding overlap stats of the region
                 overlap_stats = regionprops(overlap_mask, 'Solidity');
                 max_overlap_solidity = max([overlap_stats.Solidity]);
 
+                % if solidity of region is over max overlap keep the region
                 if r_solidity >= max_overlap_solidity
                     N = N | r_mask;
                 else
@@ -56,25 +67,32 @@ function N = segmentNuclei(EDF_image, m, s, t1, t2, d)
 
     % Post-processing
    
+    % separating all the B&W regions of the image
     CC = bwconncomp(N);
+
+    % finding the stats of those regions
     stats = regionprops(CC, imageSample, 'PixelValues', 'Perimeter', 'PixelList');
 
-
     for i = 1:CC.NumObjects
-        
-        r = stats(i);
-        r_mask = zeros(size(N));
-        r_mask(CC.PixelIdxList{i}) = 1;
-        r_mask = imdilate(r_mask, strel('disk', 1));
+    % iterating through the regions that are found previously    
+    r = stats(i);
 
-        r_intensity = mean(r.PixelValues);
-        outer_boundary = bwperim(r_mask);
-%         outer_boundary_intensity = r.PixelList & outer_boundary;
+    % initializing a mask of zeroes
+    r_mask = zeros(size(N));
+    
+    % copying the pixel index list to the mask
+    r_mask(CC.PixelIdxList{i}) = 1;
 
-%         outer_boundary = bwperim(r.Perimeter);
-       x_values = zeros(1,1);
-       y_values = zeros(1,1);
-        
+    % filtering every region with imdilate
+    r_mask = imdilate(r_mask, strel('disk', 1));
+    
+    % finding the average intensity of pixel values
+    r_intensity = mean(r.PixelValues);
+    outer_boundary = bwperim(r_mask);
+    x_values = zeros(1,1);
+    y_values = zeros(1,1);
+
+        % finding x and y values of outer boundary of each region
         for k = 1:size(outer_boundary)
             for j = 1:size(outer_boundary)
                 if (outer_boundary(k,j) == 1)
@@ -91,24 +109,24 @@ function N = segmentNuclei(EDF_image, m, s, t1, t2, d)
             end
         end
 
+        % calculating intensity difference between boundary and average
+        % intensity of r
         outer_boundary_sum = 0.0;
         for k = 1:length(x_values)
             outer_boundary_sum = outer_boundary_sum + imageSample(x_values(k), y_values(k));
         end
-
+        
         outer_boundary_intensity = outer_boundary_sum/ length(x_values);
+
+        % remove r if intensity difference is less than difference
+        % parameter d
         if (abs(r_intensity - outer_boundary_intensity)) < d
             N(CC.PixelIdxList{i}) = 0;
         end
     end
 
-[B,L] = bwboundaries(N,'noholes');
-imshow(imageSample,[])
-hold on
-for k = 1:length(B)
-   boundary = B{k};
-   plot(boundary(:,2), boundary(:,1), 'g');
-end
+    % finding boundaries of nuclei for presentation
+    [B,L] = bwboundaries(N,'noholes');
 
 % Display the original image and output binary mask
 figure;
@@ -116,6 +134,11 @@ subplot(1,2,1);
 imshow(imageSample, []);
 title('Original');
 subplot(1,2,2);
-imshow(N, []);
-title('Output Binary Mask N');
+imshow(imageSample,[])
+hold on
+for k = 1:length(B)
+   boundary = B{k};
+   plot(boundary(:,2), boundary(:,1), 'g');
+end
+title('Output Binary Mask N Boundaries');
 end
